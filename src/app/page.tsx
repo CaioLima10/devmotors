ab65eb4cd168b6248ab9a31eb7dd93f35d5e6287
Caio lima <caiohenrys50@gmail.com>
2024-04-22 14:20:29 -0300

    add sub menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
-  return <main></main>;
+import { getDataHome } from "@/utils/actions/get-data";
+import { SubMenu } from "./home/submenu";
+import { HomeData } from "@/utils/home-type";
+
+export default async function Home() {
+  const { object }: HomeData = await getDataHome();
+
+  return (
+    <main className="w-[96%] xl:w-full max-w-7xl mx-auto">
+      <SubMenu />
+    </main>
+  );
 }
