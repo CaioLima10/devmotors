0b3f0400bf00d63835c4e0646bea0e7b6128fb90
Caio lima <caiohenrys50@gmail.com>
2024-04-30 23:19:50 -0300

    feat: add id services

--- src/app/components/services/index.tsx
+++ src/app/components/services/index.tsx
@@ -5,21 +5,21 @@ export async function Services({ object }: HomeData) {
   return (
     <>
       <section
-        id="servicos"
+        id="services"
         className="flex flex-col md:flex-row items-center justify-between w-[96%] xl:w-full mx-auto my-20"
       >
         <div className="flex flex-col md:flex-row gap-20 items-center justify-between w-full">
           <article className="w-full max-w-2xl">
-            <h1 className="w-full flex items-center justify-center md:justify-start text-4xl font-bold mb-6">
+            <h1 className="w-full flex items-center justify-center md:justify-start text-4xl font-bold mb-6 uppercase">
               Sobre
             </h1>
             <p>{object.metadata.about.description}</p>
           </article>
-          <div className="relative w-80 h-96 ">
+          <div className="relative w-80 h-96 overflow-hidden rounded-xl">
             <Image
               fill
               objectFit="cover"
-              className="rounded-xl"
+              className="rounded-rounded-xl hover:duration-300 hover:scale-105 transition-all"
               src={object.metadata.about.banner.url}
               alt="banner"
             />
@@ -29,23 +29,26 @@ export async function Services({ object }: HomeData) {
 
       <section className="flex items-center justify-center mx-auto">
         <div className="w-[96%] xl:w-full">
-          <h1 className="w-full flex items-center justify-center text-4xl font-bold mb-10">
+          <h1 className="w-full flex items-center justify-center text-4xl font-bold mb-10 uppercase">
             Conheça nossos serviços
           </h1>
           <article className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10">
             {object.metadata.services.map((service, index) => (
               <>
-                <div key={index} className="relative w-50 h-60">
-                  <div>
+                <div key={index} className="relative w-50 h-60  group">
+                  <div className=" overflow-hidden rounded-xl">
                     <Image
                       fill
                       objectFit="cover"
-                      className="rounded-xl"
+                      className="rounded-xl hover:duration-300 hover:scale-105 transition-all"
                       src={service.image.url}
                       alt={service.image.url}
                     />
                   </div>
-                  <p className="flex items-center justify-center absolute -bottom-8 left-0 right-0">
+                  <p
+                    className="flex items-center bg-zinc-400 justify-center absolute 
+                  -bottom-8 left-0 right-0 overflow-auto group-hover:font-bold"
+                  >
                     {service.description}
                   </p>
                 </div>
