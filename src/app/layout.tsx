3473bd819b5bd2bd6a297fb6bdadc0141a3d56cf
Caio lima <caiohenrys50@gmail.com>
2024-05-03 17:52:40 -0300

    feat: copyright

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Header } from "./components/header";
+import { HomeData } from "@/utils/home-type";
+import { getDataHome } from "@/utils/actions/get-data";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,16 +12,21 @@ export const metadata: Metadata = {
   description: "Oficinal de carros em São Paulo",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { object }: HomeData = await getDataHome();
   return (
     <html lang="en">
       <body className={inter.className}>
         <Header />
         {children}
+        <span className="w-full flex items-center justify-center mx-auto mb-8">
+          Todos direitos reservados {object.title} @
+          {`${new Date().getFullYear()}`}
+        </span>
       </body>
     </html>
   );
